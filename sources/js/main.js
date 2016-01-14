087367206d7f992021a5a31e5e8a99652056079d
daiegon <diegorangels@gmail.com>
2016-01-14 15:38:13 -0200

    Primeira versao Mobile

---
+++ sources/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+	$('.navbar-toggle').click(function(){
+		var icone = $(this).find('span.glyphicon');
+		if(icone.hasClass('glyphicon-menu-hamburger')){
+			icone.removeClass('glyphicon-menu-hamburger');
+			icone.addClass('glyphicon-remove');
+			$(this).addClass('active');
+		} else {
+			icone.removeClass('glyphicon-remove');
+			icone.addClass('glyphicon-menu-hamburger');
+			$(this).removeClass('active');
+		}
+	});
+});
